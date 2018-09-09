fe2fbcd2fc50b49e6b0eccaedf5d7b0077a758fe
Adam Waggoner <adam.swaggoner@gmail.com>
2018-09-09 15:41:45 -0600

    Implement node connection drawing

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -16,4 +16,12 @@ public class Node : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < Connections.Count; i++)
+        {
+            Gizmos.DrawLine(transform.position, Connections[i].transform.position);
+        }
+    }
 }
