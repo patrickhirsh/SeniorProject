a91e136b3dada8e3d070a66ab7ace25f5d4db221
Adam Waggoner <adam.swaggoner@gmail.com>
2018-09-09 15:34:50 -0600

    Implement Basic Node
    Payed out a structure for nodes

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    [SerializeField]
+    public List<Node> Connections;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
