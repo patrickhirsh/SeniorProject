1e4a5cb7ebf584010013e88654ab91b6c3fced55
Patrick Hirsh <hirshpatrick@gmail.com>
2018-09-09 18:22:38 -0600

    Implemented FindShortestPath with Dijkstra's Algorithm

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -2,20 +2,99 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Node : MonoBehaviour {
+public class Node : MonoBehaviour
+{
+
+    public GameObject[] NodeObjects;
 
     [SerializeField]
     public List<Node> Connections;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // for pathfinding
+    public float Weight;       // length of shortest path from origin
+    public Node PrevNode;      // previous node in shortest path
+
+    // Use this for initialization
+    void Start ()
+    {
+        NodeObjects = GameObject.FindGameObjectsWithTag("Node");
+    }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
+
+    public List<Node> FindShortestPath(GameObject Destination)
+    {
+        // construct a transform list of all unvisited nodes
+        List<Transform> Frontier = new List<Transform>();
+        foreach (GameObject Obj in NodeObjects)
+        {
+            // first, check for parking spot nodes
+            if (Obj.GetComponent<ParkingSpotNode>() != null)
+            {
+                // only add nodes that are vacant
+                if (!Obj.GetComponent<ParkingSpotNode>().IsOccupied)
+                {
+                    Node n = Obj.GetComponent<Node>();
+                    n.Weight = int.MaxValue;
+                    n.PrevNode = null;
+                    Frontier.Add(Obj.transform);
+                }
+            }
+
+            // otherwise, always add the node
+            else
+            {
+                Node n = Obj.GetComponent<Node>();
+                n.Weight = int.MaxValue;
+                n.PrevNode = null;
+                Frontier.Add(Obj.transform);
+            }
+        }
+
+        // set start node and begin exploring
+        this.Weight = 0;
+        while(Frontier.Count > 0)
+        {
+            // Sort Frontier by weight in ascending order and select the lowest weight as current
+            Frontier.Sort((x, y) => x.GetComponent<Node>().Weight.CompareTo(y.GetComponent<Node>().Weight));
+            Transform Current = Frontier[0];
+            Frontier.Remove(Current);
+
+            // 
+            Node CurrentNode = Current.GetComponent<Node>();
+            foreach (Node Connection in CurrentNode.Connections)
+            {
+                // only explore unexplored nodes
+                if (Frontier.Contains(Connection.gameObject.transform))
+                {
+                    // calculate new distance
+                    float Distance = Vector3.Distance(Connection.gameObject.transform.position, Current.position);
+                    Distance = CurrentNode.Weight + Distance;
+
+                    // if we've found a shorter path, update
+                    if (Distance < Connection.Weight)
+                    {
+                        Connection.Weight = Distance;
+                        Connection.PrevNode = CurrentNode;
+                    }
+                }
+            }
+        }
+
+        List<Node> output = new List<Node>();
+        Node Traverse = Destination.GetComponent<Node>();
+        output.Add(Traverse);
+
+        while (Traverse.PrevNode != null)
+            output.Add(PrevNode);
+
+        output.Reverse();
+        return output;
+    }
 
     private void OnDrawGizmos()
     {
@@ -24,4 +103,7 @@ public class Node : MonoBehaviour {
             Gizmos.DrawLine(transform.position, Connections[i].transform.position);
         }
     }
+
+
+    
 }
