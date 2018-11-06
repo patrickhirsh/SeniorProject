30c2edb6928dfeef7c3cde3fd0751b496210a055
Patrick Hirsh <hirshpatrick@gmail.com>
2018-11-05 23:07:05 -0700

    Began skeleton for AI system

---
+++ Assets/_scripts/AI/AIManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Level
+{
+    public class AIManager : MonoBehaviour
+    {
+        #region Singleton
+        private static PathfindingManager _instance;
+        public static PathfindingManager Instance => _instance ?? (_instance = Create());
+
+        private static PathfindingManager Create()
+        {
+            GameObject singleton = FindObjectOfType<PathfindingManager>()?.gameObject;
+            if (singleton == null) singleton = new GameObject { name = typeof(PathfindingManager).Name };
+            singleton.AddComponent<PathfindingManager>();
+            return singleton.GetComponent<PathfindingManager>();
+        }
+        #endregion
+
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
