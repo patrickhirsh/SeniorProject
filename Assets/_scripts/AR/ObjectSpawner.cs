293f68d38b4becbd4321a7ef0e259c168de371f1
Adam Waggoner <adam.swaggoner@gmail.com>
2018-11-15 17:56:55 -0700

    Add commented out AR plane spawner

---
+++ Assets/_scripts/AR/ObjectSpawner.cs
@@ -0,0 +1,32 @@
+﻿/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ObjectSpawner : MonoBehaviour
+{
+
+    ARPlaneManager PlaneManager;
+    public GameObject SpawnPrefab;
+
+    // Use this for initialization
+    void Start()
+    {
+        PlaneManager = gameObject.GetComponent<ARPlaneManager>();
+        PlaneManager.planeAdded += OnPlaneAdded;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnPlaneAdded(ARPlaneAddedEventArgs args)
+    {
+        Debug.Log(args.plane.boundedPlane.Center);
+
+        Instantiate(SpawnPrefab, args.plane.boundedPlane.Center, new Quaternion(0, 0, 0, 1));
+    }
+}*/
