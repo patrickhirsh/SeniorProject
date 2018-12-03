abb46d2bc1f5b4b103323850f361c19ae768dd22
greyvest <greyvestanderson@gmail.com>
2018-12-02 17:40:52 -0700

    Billboard script / Pin object

---
+++ Assets/_scripts/Utility/Billboard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Billboard : MonoBehaviour
+{
+    public Camera cam;
+    // Use this for initialization
+    void Start()
+    {
+        //this.GetComponent<Rigidbody>().centerOfMass = new Vector3(0, -3, 0);
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+      transform.LookAt(cam.transform);
+
+    }
+}
