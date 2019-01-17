3068c4741773a97967d1033b2595ae7450c8926e
greyvest <greyvestanderson@gmail.com>
2019-01-16 20:56:13 -0700

    Started making building class

---
+++ Assets/_scripts/Entities/Building.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour {
+
+    /// <summary>
+    /// Location that vehicles will deliver passengers to
+    /// </summary>
+    public Level.Route DeliveryLocation;
+
+    public enum BuildingColors {Red, Green, Blue, Yellow, Purple, Orange }
+
+    public BuildingColors BuildingColor;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+
+}
--- Assets/_scripts/Passenger/Passenger.cs
+++ Assets/_scripts/Passenger/Passenger.cs
@@ -9,6 +9,8 @@ namespace Level
         public Terminal StartTerminal;
         public Terminal DestinationTerminal;
 
+        public enum Color {Red, Green, Blue, Yellow, Purple, Orange}
+
         public int SearchDepth;
 
         public Pin PassengerPickupReticle;
