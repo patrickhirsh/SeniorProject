f4d49cefe4698208c655d7bf9ba79c50a12e3e12
Patrick Hirsh <hirshpatrick@gmail.com>
2019-01-22 10:58:09 -0700

    Added functionality for spawning enemy vehicles when passengers time out

---
+++ Assets/_scripts/Vehicle/Managers/EnemyVehicleManager.cs
@@ -0,0 +1,29 @@
+﻿using Level;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyVehicleManager : VehicleManager
+{
+    public override void VehicleTaskCallback(TaskType type, Vehicle vehicle, bool exitStatus)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    /// <summary>
+    /// given a spawn point and a vehicle prefab, spawns a new instance of vehiclePrefab at "spawnPoint" with "destination"
+    /// </summary>
+    private void SpawnVehicle(Connection spawnPoint, Connection destination, GameObject vehiclePrefab)
+    {
+        // instantiate the new vehicle
+        var vehicle = Instantiate(vehiclePrefab, spawnPoint.transform.position, Quaternion.identity, transform).GetComponent<Vehicle>();
+        vehicle.Manager = this;
+
+        // obtain a path to the destination
+        Queue<Connection> path = new Queue<Connection>();
+        PathfindingManager.Instance.GetPath(spawnPoint, destination, out path);
+
+        // assign the pathing task to this new vehicle
+        vehicle.AssignTask(new VehicleTask(TaskType.ActiveAi, path, VehicleTaskCallback));
+    }
+}
