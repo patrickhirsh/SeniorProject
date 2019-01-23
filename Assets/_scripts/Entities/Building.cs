60a70d9eb29067ae879ff3721eb93a6037c9c0e3
greyvest <greyvestanderson@gmail.com>
2019-01-23 13:07:09 -0700

    Made materials

--- Assets/_scripts/Entities/Building.cs
+++ Assets/_scripts/Entities/Building.cs
@@ -9,13 +9,44 @@ public class Building : MonoBehaviour {
     /// </summary>
     public Level.Route DeliveryLocation;
 
+    public Material Red;
+    public Material Blue;
+    public Material Green;
+    public Material Yellow;
+    public Material Purple;
+    public Material Orange;
+
+
     public enum BuildingColors {Red, Green, Blue, Yellow, Purple, Orange }
 
     public BuildingColors BuildingColor;
 
 	// Use this for initialization
 	void Start () {
-		
+        switch(BuildingColor)
+        {
+            case BuildingColors.Red:
+                this.gameObject.GetComponent<Renderer>().material = Red;
+                break;
+            case BuildingColors.Green:
+                this.gameObject.GetComponent<Renderer>().material = Green;
+                break;
+            case BuildingColors.Blue:
+                this.gameObject.GetComponent<Renderer>().material = Blue;
+                break;
+            case BuildingColors.Yellow:
+                this.gameObject.GetComponent<Renderer>().material = Yellow;
+                break;
+            case BuildingColors.Purple:
+                this.gameObject.GetComponent<Renderer>().material = Purple;
+                break;
+            case BuildingColors.Orange:
+                this.gameObject.GetComponent<Renderer>().material = Orange;
+                break;
+            default:
+
+                break;
+        }
 	}
 	
 	// Update is called once per frame
