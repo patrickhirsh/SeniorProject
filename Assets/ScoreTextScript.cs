6dbd4646ff6fa3de9e4321fc2becb4d56c3fbb59
greyvest <greyvestanderson@gmail.com>
2019-01-26 16:27:03 -0700

    Added colliders to all buildings, halfway done with 3d scoring text

---
+++ Assets/ScoreTextScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreTextScript : MonoBehaviour {
+
+    ScoreManagerScript SM;
+
+	// Use this for initialization
+	void Start () {
+        SM = GameObject.FindObjectOfType<ScoreManagerScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
