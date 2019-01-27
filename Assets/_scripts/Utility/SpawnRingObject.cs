7703cb7b1503075ad3173483ff4d9a6607c7daac
Adam Waggoner <adam.swaggoner@gmail.com>
2019-01-27 16:12:49 -0700

    Implement Ring Spawning Script

--- Assets/_scripts/Utility/SpawnRingObject.cs
+++ Assets/_scripts/Utility/SpawnRingObject.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class SpawnRingObject : MonoBehaviour {
 
+    [SerializeField]
+    private GameObject _RingObject;
+
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void SpawnRing(Vector3 position, Color RingColor, float RingSpeed)
+    {
+        GameObject spawnedObj = Instantiate(_RingObject, position, Quaternion.identity);
+
+        Material ringMat = spawnedObj.GetComponent<Renderer>().material;
+        ringMat.SetColor("_Color", RingColor);
+        ringMat.SetFloat("_Speed", RingSpeed);
+    }
 }
