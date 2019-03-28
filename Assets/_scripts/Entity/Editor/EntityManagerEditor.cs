5573c3894fe8de69eef69ae0ea5b3e2214d899c8
JadenH <jadenholladay@gmail.com>
2019-03-27 22:16:46 -0600

    fix null routes on entity manager

--- Assets/_scripts/Entity/Editor/EntityManagerEditor.cs
+++ Assets/_scripts/Entity/Editor/EntityManagerEditor.cs
@@ -9,6 +9,10 @@ namespace Level
     [CanEditMultipleObjects]
     public class EntityManagerEditor : Editor
     {
+        public float secs = 10f;
+        public float startVal = 0f;
+        public float progress = 0f;
+
         public override void OnInspectorGUI()
         {
             DrawDefaultInspector();
@@ -20,18 +24,26 @@ namespace Level
             if (myTarget.Entities != null) EditorGUILayout.LabelField($"{myTarget.Entities.Length} Entities");
             if (myTarget.Connections != null) EditorGUILayout.LabelField($"{myTarget.Connections.Length} Connections");
             if (myTarget.Routes != null) EditorGUILayout.LabelField($"{myTarget.Routes.Length} Routes");
-            if (myTarget.Routes != null) EditorGUILayout.LabelField($"{myTarget.Routes.Sum(route => route.Terminals?.Length ?? 0)} Terminals");
+            if (myTarget.Routes != null) EditorGUILayout.LabelField($"{myTarget.Routes.Where(route => route != null).Sum(route => route.Terminals?.Length ?? 0)} Terminals");
 
-            if (myTarget.Entities == null || myTarget.Connections == null || myTarget.Routes == null)
+            if (myTarget.Entities == null || myTarget.Connections == null || myTarget.Routes == null ||
+                myTarget.Entities.Any(AreNull) || myTarget.Connections.Any(AreNull) || myTarget.Routes.Any(AreNull))
             {
-                EditorGUILayout.LabelField($"Entity Manager needs to be baked!");
+                Debug.Log("Baking Entity Manager Automatically...");
+                myTarget.Bake();
             }
             EditorGUILayout.Space();
             if (GUILayout.Button("Bake Level"))
             {
+                Debug.Log("Baking Entity Manager...");
                 myTarget.Bake();
                 //NeutralVehicleManager.Instance.bakeNeutralPaths();
             }
+        }
+
+        private bool AreNull(Object obj)
+        {
+            return obj == null;
         }
 
         private static void PropertyField(SerializedProperty myIterator)
