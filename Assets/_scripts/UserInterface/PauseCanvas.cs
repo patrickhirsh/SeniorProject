b9538edeeb0042af1617fb1b0a22e66e51bff996
greyvest <greyvestanderson@gmail.com>
2019-04-13 13:09:35 -0600

    Pause menu, fixed vehicle collisions and neutral vehicle prefabs, etc.

---
+++ Assets/_scripts/UserInterface/PauseCanvas.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseCanvas : MonoBehaviour
+{
+
+    public GameObject Children;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TurnOn()
+    {
+        Children.SetActive(true);
+    }
+
+    public void TurnOff()
+    {
+        Children.SetActive(false);
+    }
+
+    public void HandleRestartButton()
+    {
+        Broadcaster.Broadcast(GameEvent.Reset);
+        GameManager.SetGameState(GameState.LevelRePlacement);
+        TurnOff();
+    }
+}
