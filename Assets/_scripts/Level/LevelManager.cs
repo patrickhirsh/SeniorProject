9a99ffc637ca305cc89f6b3de0419c623664401b
greyvest <greyvestanderson@gmail.com>
2019-04-15 13:43:05 -0600

    A bunch of small changes

--- Assets/_scripts/Level/LevelManager.cs
+++ Assets/_scripts/Level/LevelManager.cs
@@ -13,8 +13,8 @@ public class LevelManager : Singleton<LevelManager>
     public Level CurrentLevel;
     //Bool for firing fireworks
     private bool fireworks = false;
-				//timer for firing fireworks
-				private float fTimer = 0;
+    //timer for firing fireworks
+    private float fTimer = 0;
     //interval at which to fire fireworks
     public float FireworkInterval;
     //interval variance for firing fireworks
@@ -42,10 +42,10 @@ public class LevelManager : Singleton<LevelManager>
             ParticleManager.Instance.GenerateFirework(randPos, randomColor);
 
             fTimer += FireworkInterval + Random.Range(-FireworkIntervalVariance, FireworkIntervalVariance);
-								}
-								fTimer -= .01f;
+        }
+        fTimer -= .01f;
 
-				}
+    }
 
     private void LaunchSuccessFireworks(GameEvent action)
     {
@@ -76,19 +76,18 @@ public class LevelManager : Singleton<LevelManager>
     }
 
 
-    public void TransitionLevelPrefab(GameObject levelprefab, AudioClip NewLayer1, AudioClip NewLayer2, AudioClip NewLayer3)
+    public void TransitionLevelPrefab(GameObject levelprefab)
     {
         fireworks = false;
         //Destroy old level object
         var oldLevel = CurrentLevel.gameObject;
 
         //Switch music to music of new level
-        Osborne_AudioManager.Instance.SwitchLevels(NewLayer1, NewLayer2, NewLayer3);
-        
+
         //Spawn and place new level prefab in that spot.
         CurrentLevel = Instantiate(levelprefab, oldLevel.transform.position, oldLevel.transform.rotation).GetComponent<Level>();
         Destroy(oldLevel);
-        CurrentLevel.EntityController.Initialize();
+        CurrentLevel.Initialize();
 
         //Notify controllers of the new gamestate
         Broadcaster.Broadcast(GameEvent.GameStateChanged);
@@ -100,17 +99,23 @@ public class LevelManager : Singleton<LevelManager>
         fireworks = false;
         //Destroy old level object
         var oldLevel = CurrentLevel.gameObject;
-
+        
         //Switch music to music of new level
         Osborne_AudioManager.Instance.SwitchLevels(newLevel.NewLayer1, newLevel.NewLayer2, newLevel.NewLayer3);
 
         //Spawn and place new level prefab in that spot.
         CurrentLevel = Instantiate(newLevel.RepresentedLevel, oldLevel.transform.position, oldLevel.transform.rotation).GetComponent<Level>();
         Destroy(oldLevel);
-        CurrentLevel.Initialize();
 
+        
+        //Broadcaster.Broadcast(GameEvent.Reset);
+        CurrentLevel.Initialize();
         //Notify controllers of the new gamestate
-        Broadcaster.Broadcast(GameEvent.GameStateChanged);
+        //Broadcaster.Broadcast(GameEvent.GameStateChanged);
         Broadcaster.Broadcast(GameEvent.LevelChange);
+    }
+
+    public void ResetCurrentLevel(){
+        CurrentLevel.Initialize();
     }
 }
--- Assets/_scripts/UserInterface/PauseCanvas.cs
+++ Assets/_scripts/UserInterface/PauseCanvas.cs
@@ -48,4 +48,5 @@ public class PauseCanvas : MonoBehaviour
         GameManager.SetGameState(GameState.LevelRePlacement);
         TurnOff();
     }
+
 }
